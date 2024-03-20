9aae6db1e3485f034d9374a76fe36d4f5e9f23d7
Misha <misha.babarov@gmail.com>
2024-03-20 22:00:44 +0700

    Add base interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use text_io::read;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Выберите фигуру:");
+    println!("1. Прямоугольник");
+    println!("2. Треугольник");
+    println!("3. Трапеция");
+    println!("4. Окружность");
+
+    let choice: u8 = read!();
+
+    if choice == 1 {
+        println!("Вы выбрали прямоугольник"); 
+    }
 }
