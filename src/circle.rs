851e0d95d6e257475b3a79af4b9332525a5ab582
Nikita <nikitosproskyha@gmail.com>
2024-03-22 00:25:24 +0700

    Swap the lines

--- src/circle.rs
+++ src/circle.rs
@@ -4,11 +4,11 @@ use std::f64::consts::PI;
 pub fn process_circle() {
     println!("Введите радиус окружности:");
     let radius: f64 = read!();
+    println!("Введите угол сектора (в градусах):");
+    let angle: f64 = read!();
 
     let circumference: f64 = 2.0 * PI * radius;
     let area: f64 = PI * radius.powi(2);
-    println!("Введите угол сектора (в градусах):");
-    let angle: f64 = read!();
     let sector_area = PI * radius.powi(2) * (angle / 360.0);
 
     println!("Длина окружности: {}", circumference);
