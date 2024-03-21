51cbe4fa009502aee3d99049b910ba26cbb21b0f
root <root@HOME-PC>
2024-03-21 20:18:03 +0300

    Added area

--- src/circle.rs
+++ src/circle.rs
@@ -4,7 +4,9 @@ use std::f64::consts::PI;
 pub fn process_circle() {
     println!("Введите радиус окружности:");
     let radius: f64 = read!();
+    let area: f64 = PI * radius.powi(2);
 
     let circumference: f64 = 2.0 * PI * radius;
     println!("Длина окружности: {}", circumference);
+    println!("Площадь круга: {}", area);
 }
