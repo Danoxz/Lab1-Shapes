bb854336ee57fa1ddbe645f9f2cc49ee23e74e47
root <root@HOME-PC>
2024-03-21 20:19:49 +0300

    Added sector area

--- src/circle.rs
+++ src/circle.rs
@@ -4,9 +4,14 @@ use std::f64::consts::PI;
 pub fn process_circle() {
     println!("Введите радиус окружности:");
     let radius: f64 = read!();
-    let area: f64 = PI * radius.powi(2);
 
     let circumference: f64 = 2.0 * PI * radius;
+    let area: f64 = PI * radius.powi(2);
+    println!("Введите угол сектора (в градусах):");
+    let angle: f64 = read!();
+    let sector_area = PI * radius.powi(2) * (angle / 360.0);
+
     println!("Длина окружности: {}", circumference);
     println!("Площадь круга: {}", area);
+    println!("Площадь сектора: {}", sector_area);
 }
