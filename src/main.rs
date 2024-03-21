4c5223bf7c33324519eacbf988319012dd74d7ee
Misha <misha.babarov@gmail.com>
2024-03-22 00:29:13 +0700

    fix some bugs

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,20 @@ mod rectangle;
 
 use text_io::read;
 fn main() {
-    println!("Выберите фигуру:");
-    println!("1. Прямоугольник");
-    println!("2. Трапеция");
-    println!("3. Окружность");
-    println!("4. Выход");
-    let choice: u8 = read!();
+    loop {
+        println!("Выберите фигуру:");
+        println!("1. Прямоугольник");
+        println!("2. Трапеция");
+        println!("3. Окружность");
+        println!("4. Выход");
+        let choice: u8 = read!();
 
-    match choice {
-        1 => rectangle::process_rectangle(),
-        2 => trapezoid::process_trapezoid(),
-        3 => circle::process_circle(),
-        4 => break,
-        _ => println!("Неверный выбор. Попробуйте еще раз."),
+        match choice {
+            1 => rectangle::process_rectangle(),
+            2 => trapezium::process_trapezium(),
+            3 => circle::process_circle(),
+            4 => break,
+            _ => println!("Неверный выбор. Попробуйте еще раз.")
         }
+    }
 }
