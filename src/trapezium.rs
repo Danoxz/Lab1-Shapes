84e4f408b250365dd06334a6c8c7ab0a0f0335f3
Misha <misha.babarov@gmail.com>
2024-03-21 21:40:18 +0700

    Can also midline

--- src/trapezium.rs
+++ src/trapezium.rs
@@ -7,10 +7,12 @@ pub fn process_trapezium() {
     let side: f64 = read!();
 
     let perimeter: f64 = base1 + base2 + side * 2.0;
-    let height: f64 = (side.powi(2) - ((base1 - base2) / 2.0).abs().powi(2)).sqrt();
+    let midline: f64 = (base1 - base2).abs() / 2.0;
+    let height: f64 = (side.powi(2) - midline.powi(2)).sqrt();
 
     let area = 0.5 * (base1 + base2) * height;
 
     println!("Периметр: {}", perimeter);
     println!("Площадь: {}", area);
+    println!("Средняя линия: {}", midline);
 }
