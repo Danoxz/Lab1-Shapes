59b8f941794746e626f85f86071ed831e8f1bb55
Misha <misha.babarov@gmail.com>
2024-03-21 21:11:22 +0700

    Now he can calculate perimeter

--- src/trapezium.rs
+++ src/trapezium.rs
@@ -1,5 +1,14 @@
 use text_io::read;
 
 pub fn process_trapezium() {
-    println!("process_trapezoid works");
+    println!("Введите длины оснований и боковой стороны трапеции:");
+    let base1: f64 = read!();
+    let base2: f64 = read!();
+    let side: f64 = read!();
+
+    let perimeter: f64 = base1 + base2 + side * 2.0;
+
+
+
+    println!("Периметр: {}", perimeter);
 }
