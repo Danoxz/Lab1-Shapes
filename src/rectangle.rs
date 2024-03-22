e22ee4edf37001b0b794ee6ceb1a3ab2927d6cef
Misha <misha.babarov@gmail.com>
2024-03-22 10:13:40 +0700

    Rectangleblablabla

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -5,11 +5,15 @@ pub fn process_rectangle() {
     let length: f64 = read!();
     let width: f64 = read!();
 
-    let perimeter: f64 = 2.0 * (length + width);
-    let area: f64 = length * width;
-    let diagonal: f64 = (length.powi(2) + width.powi(2)).sqrt();
-
-    println!("Периметр: {}", perimeter);
-    println!("Площадь: {}", area);
-    println!("Длина диагонали: {}", diagonal);
+    if length < 0.0 || width < 0.0{
+        println!("Неккоректное значение");
+    } else {
+        let perimeter: f64 = 2.0 * (length + width);
+        let area: f64 = length * width;
+        let diagonal: f64 = (length.powi(2) + width.powi(2)).sqrt();
+    
+        println!("Периметр: {}", perimeter);
+        println!("Площадь: {}", area);
+        println!("Длина диагонали: {}", diagonal);
+    }
 }
